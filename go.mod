fcf4d8fb5f9f8c52cd9e0734115bc33f099801fc
David Antunes <dmm.antunes@campus.fct.unl.pt>
2024-09-11 22:36:08 +0100

    Add logic to respond to metrics-manager

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/David-Antunes/network-emulation-rtt-program
 
 go 1.23.1
+
+require (
+	github.com/David-Antunes/network-emulation-proxy v1.1.1-0.20240911193246-c7b754b040e5 // indirect
+	github.com/cilium/ebpf v0.16.0 // indirect
+	github.com/david-antunes/xdp v1.0.1 // indirect
+	github.com/google/gopacket v1.1.19 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/vishvananda/netlink v1.3.0 // indirect
+	github.com/vishvananda/netns v0.0.4 // indirect
+	golang.org/x/exp v0.0.0-20240909161429-701f63a606c0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,74 @@
 package main
 
+import (
+	"bytes"
+	"encoding/json"
+	"net"
+	"strings"
+	"time"
+
+	api "github.com/David-Antunes/network-emulation-proxy/api"
+)
 
 func main() {
+	listenAddr, err := net.ResolveUDPAddr("udp4", ":8000")
+	if err != nil {
+		panic(err)
+	}
+	port, err := net.ListenUDP("udp4", listenAddr)
+	if err != nil {
+		panic(err)
+	}
+
+	ief, err := net.InterfaceByName("eth0")
+	if err != nil {
+		panic(err)
+	}
+	addrs, err := ief.Addrs()
+	if err != nil {
+		panic(err)
+	}
+	splitAddr := strings.Split(addrs[0].String(), ".")
+
+	if len(splitAddr) != 4 {
+		panic("something went wrong with Ip address")
+	}
+
+	broadcastIp := splitAddr[0] + "." + splitAddr[1] + "." + splitAddr[2] + ".255:8000"
+
+	conn, err := net.Dial("udp4", broadcastIp)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		buf := make([]byte, 1024)
+		size, err := port.Read(buf)
+
+		if err != nil {
+			panic(err)
+		}
+
+		buf = buf[:size]
+
+		resp := &api.RTTRequest{}
+		d := json.NewDecoder(bytes.NewReader(buf))
+		err = d.Decode(resp)
+		if err != nil {
+			panic(err)
+		}
+
+		resp.ReceiveTime = time.Now()
+		resp.TransmitTime = time.Now()
+
+		req, err := json.Marshal(resp)
+
+		if err != nil {
+			panic(err)
+		}
+
+		conn.Write(req)
+	}
 
 }
